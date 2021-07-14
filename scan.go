aa773f6a7e45882fdb93fcddd6d73ed53b26851f
Matsue Kenta <40712363+matsuei@users.noreply.github.com>
2021-07-14 11:19:31 +0900

    Update scan.go

--- scan.go
+++ scan.go
@@ -1,28 +1,28 @@
 import (
   "bufio"
-	"fmt"
+  "fmt"
 )
 
 const (
-	initialBufSize = 10000
-	maxBufSize     = 1000000
-	mod            = 1e9 + 7
+  initialBufSize = 10000
+  maxBufSize     = 1000000
+  mod            = 1e9 + 7
 )
 
 var (
-	sc *bufio.Scanner = func() *bufio.Scanner {
-		sc := bufio.NewScanner(os.Stdin)
-		buf := make([]byte, initialBufSize)
-		sc.Buffer(buf, maxBufSize)
-		return sc
-	}()
+  sc *bufio.Scanner = func() *bufio.Scanner {
+    sc := bufio.NewScanner(os.Stdin)
+    buf := make([]byte, initialBufSize)
+    sc.Buffer(buf, maxBufSize)
+    return sc
+  }()
 )
 
 func main() {
-	sc.Split(bufio.ScanWords)
-	N := nextInt()
-	L := make([]int, N)
-	for i := 0; i < N; i++ {
-		L[i] = nextInt()
-	}
+  sc.Split(bufio.ScanWords)
+  N := nextInt()
+  L := make([]int, N)
+  for i := 0; i < N; i++ {
+    L[i] = nextInt()
+  }
 }
