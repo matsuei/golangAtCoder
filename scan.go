c6bef42760b92534b8ab29ad80842c8968162c30
Matsue Kenta <40712363+matsuei@users.noreply.github.com>
2021-07-14 11:18:22 +0900

    Create scan.go

---
+++ scan.go
@@ -0,0 +1,28 @@
+import (
+	"bufio"
+	"fmt"
+)
+
+const (
+	initialBufSize = 10000
+	maxBufSize     = 1000000
+	mod            = 1e9 + 7
+)
+
+var (
+	sc *bufio.Scanner = func() *bufio.Scanner {
+		sc := bufio.NewScanner(os.Stdin)
+		buf := make([]byte, initialBufSize)
+		sc.Buffer(buf, maxBufSize)
+		return sc
+	}()
+)
+
+func main() {
+	sc.Split(bufio.ScanWords)
+	N := nextInt()
+	L := make([]int, N)
+	for i := 0; i < N; i++ {
+		L[i] = nextInt()
+	}
+}
