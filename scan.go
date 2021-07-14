ef3a9b54cba02534f07a7b1f9e0de3c154bf84ba
Matsue Kenta <40712363+matsuei@users.noreply.github.com>
2021-07-14 11:21:16 +0900

    Update scan.go

--- scan.go
+++ scan.go
@@ -18,6 +18,19 @@ var (
   }()
 )
 
+func nextStr() string {
+  sc.Scan()
+  return sc.Text()
+}
+func nextInt() int {
+  sc.Scan()
+  i, e := strconv.Atoi(sc.Text())
+  if e != nil {
+    panic(e)
+  }
+  return i
+}
+
 func main() {
   sc.Split(bufio.ScanWords)
   N := nextInt()
