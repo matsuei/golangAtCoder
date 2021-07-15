4724315eefc346c1ae06482222ea8004bb85cbc0
Matsue Kenta <40712363+matsuei@users.noreply.github.com>
2021-07-16 08:56:48 +0900

    Update scan.go

--- scan.go
+++ scan.go
@@ -1,41 +1,29 @@
+package main
+
 import (
-  "bufio"
-  "fmt"
+    "bufio"
+    "fmt"
+    "os"
+    "strconv"
 )
 
-const (
-  initialBufSize = 10000
-  maxBufSize     = 1000000
-  mod            = 1e9 + 7
-)
+var sc = bufio.NewScanner(os.Stdin)
 
-var (
-  sc *bufio.Scanner = func() *bufio.Scanner {
-    sc := bufio.NewScanner(os.Stdin)
-    buf := make([]byte, initialBufSize)
-    sc.Buffer(buf, maxBufSize)
-    return sc
-  }()
-)
-
-func nextStr() string {
-  sc.Scan()
-  return sc.Text()
-}
 func nextInt() int {
-  sc.Scan()
-  i, e := strconv.Atoi(sc.Text())
-  if e != nil {
-    panic(e)
-  }
-  return i
+    sc.Scan()
+    i, e := strconv.Atoi(sc.Text())
+    if e != nil {
+        panic(e)
+    }
+    return i
 }
 
 func main() {
-  sc.Split(bufio.ScanWords)
-  N := nextInt()
-  L := make([]int, N)
-  for i := 0; i < N; i++ {
-    L[i] = nextInt()
-  }
+    sc.Split(bufio.ScanWords)
+    n := nextInt()
+    x := 0
+    for i := 0; i < n; i++ {
+        x += nextInt()
+    }
+    fmt.Println(x)
 }
