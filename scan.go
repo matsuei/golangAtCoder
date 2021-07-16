677552e19a5f077e06664575ea7a8194a235544e
Matsue Kenta <40712363+matsuei@users.noreply.github.com>
2021-07-16 20:43:55 +0900

    Update scan.go

--- scan.go
+++ scan.go
@@ -20,10 +20,10 @@ func nextInt() int {
 
 func main() {
     sc.Split(bufio.ScanWords)
-    n := nextInt()
+    x := make([]int, n)
     x := 0
     for i := 0; i < n; i++ {
-        x += nextInt()
+        x[i] = nextInt()
     }
     fmt.Println(x)
 }
